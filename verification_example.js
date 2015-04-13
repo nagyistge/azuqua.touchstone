b82bbe564faaf094ca825605cca7b8487ff616c4
Lito Nicolai <lito@azuqua.com>
2015-04-13 14:41:51 -0700

    package

--- verification_example.js
+++ verification_example.js
@@ -1,5 +1,5 @@
 var stream = require('stream');
-var syntaxCheck = require('./testing_framework/syntax');
-var channel = JSON.stringify(require('./channels/marketo.json'));
+var syntaxCheck = require('./syntax');
+var channel = JSON.stringify(require('../channels/marketo.json'));
 
 syntaxCheck(channel, true);
